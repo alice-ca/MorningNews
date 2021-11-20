b0aacf852504dc216cecf4589d76bf456c673e7c
Alice Cretin-Anciaux <alice_ca@hotmail.fr>
2021-11-20 11:53:09 +0100

    finalpush

--- reactapp/src/reducers/articles.js
+++ reactapp/src/reducers/articles.js
@@ -5,12 +5,6 @@ export default function (wishList = [], action) {
 
     } else if (action.type == 'addArticle') {
         let wishListCopy = [...wishList]
-        /*let findArticle = false;
-        for(let i=0;i<wishListCopy.length;i++){
-            if(wishListCopy[i].title == action.articleLiked.title){
-                findArticle = true
-            }
-        }*/
 
         if (!wishListCopy.some(article => article.title === action.articleLiked.title)) {
             wishListCopy.push(action.articleLiked)
@@ -18,17 +12,6 @@ export default function (wishList = [], action) {
         return wishListCopy
 
     } else if (action.type == 'deleteArticle') {
-        /*let wishListCopy = [...wishList]
-        var position = null
-
-        for (let i = 0; i < wishListCopy.length; i++) {
-            if (wishListCopy[i].title == action.title) {
-                position = i
-            }
-        }
-        if (position != null) {
-            wishListCopy.splice(position, 1)
-        }*/
 
         return wishList.filter(article => article.title !== action.title)
 
